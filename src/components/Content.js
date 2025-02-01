794a61d0fade5dcd3ac3a0478632331c180e47f7
Meera Bhola <meerabhola3@gmail.com>
2025-02-01 01:25:59 -0500

    init files and started

---
+++ src/components/Content.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const mainContent = () => {
+    return (
+        <div>
+            <p><em>The Tortoise and the Hare</em> is a tale as old as time. This story teaches patience through a piece of fiction. In society, we are taught to go fast, yet this story reminds us we can take a break sometimes and still win.</p>
+
+            <p>One day, the Tortoise and the Hare decide to race. The Hare, cocky and self-assured, taunts the Tortoise. The Hare has seen the Tortoise move before and knows he is very slow. The Tortoise on the other hand tends to speed through the forest.</p>
+
+            <p>The race is about to begin! The animals line up. People placing big bets that the Hare is bound to win the race. The Hare starts out strong, however, they tire out quickly. The Tortoise on the other hands keeps going. Halfway through the race, the Tortoise decides he's tired and has enough of a lead to take a nap and still beat the tortoise.</p>
+
+            <p>The Hare decides to sleep while the Tortoise keeps on going. After all, he is much farther behind, there is no way he can catch up to the where the Hare is before he wakes up. The Hare accidentally oversleeps! While he is sleeping, the Tortoise inched closer and closer to the finish line. With only 0.5 inch left, the Tortois crosses the finish line first while the Hare is racing to catch up. The Tortoise has won the race, shocking everyone in the crowd! The Hare, bewildered, struggles to catch his breath as he processes how he just lost.The Tortoise celebrates by going home and napping after his big, hard-earned win.</p>
+        </div>
+    )
+};
+
+export default mainContent
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <h1>The Tortoise and the Hare</h1>
+    )
+};
+export default Title
